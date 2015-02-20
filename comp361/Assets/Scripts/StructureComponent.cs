5d91288c72953c04592d9d8d8b2ce68643c13cc5
Francois-Xavier Lemire <francois-xavier.lemire@mail.mcgill.ca>
2015-02-19 19:47:49 -0500

    StructureComponent implemented // TileComponent placeholder

---
+++ comp361/Assets/Scripts/StructureComponent.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+enum StructureType
+{
+	NONE,
+	TOMBSTONE,
+	WATCHTOWER
+}
+
+public class StructureComponent : MonoBehaviour {
+
+	StructureType _structureType;
+	TileComponent _location;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	StructureType getStructureType() {
+		return _structureType;
+	}
+
+	void setStructureType(StructureType structureType) {
+		_structureType = structureType;
+	}
+
+	TileComponent getLocation()
+	{
+		return _location;
+	}
+
+	void setLocation(TileComponent location)
+	{
+		_location = location;
+	}
+}
