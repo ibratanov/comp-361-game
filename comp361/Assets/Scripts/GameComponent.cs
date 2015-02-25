a00970b99d6e682c488474dc7b93532ae206617a
Francois-Xavier Lemire <francois-xavier.lemire@mail.mcgill.ca>
2015-02-24 20:14:22 -0500

    GameComponent implemented

--- comp361/Assets/Scripts/GameComponent.cs
+++ comp361/Assets/Scripts/GameComponent.cs
@@ -3,6 +3,49 @@ using System.Collections;
 
 public class GameComponent : MonoBehaviour {
 
+	/*********************
+	 *     ATTRIBUTES    *
+	 ********************/
+
+	PlayerComponent _currentPlayer;
+	PlayerComponent[] _participants;
+	PlayerComponent[] _remainingPlayers;
+	TileComponent[] _mapTiles;
+
+	/*********************
+	 *  GETTERS/SETTERS  *
+	 ********************/
+
+	void setCurrentPlayer(PlayerComponent currentPlayer) {
+		_currentPlayer = currentPlayer;
+	}
+
+	PlayerComponent[] getRemainingPlayers() {
+		return _remainingPlayers;
+	}
+
+	void setRemainingPlayers(PlayerComponent[] remainingPlayers) {
+		_remainingPlayers = remainingPlayers;
+	}
+
+	/*********************
+	 *      METHODS      *
+	 ********************/
+
+	void endGame() {
+		/* TODO */
+	}
+
+	void newGame(PlayerComponent[] participants) {
+		/* TODO */
+	}
+
+	void removePlayer(PlayerComponent player) {
+		/* TODO */
+	}
+
+
+
 	// Use this for initialization
 	void Start () {
 	
