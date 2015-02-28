41752813a49e825c98672f0b51f36453af71ef6e
reetah <rita.lu410@gmail.com>
2015-02-28 14:10:32 -0500

    Implemented newGame

--- comp361/Assets/Scripts/classComponents/GameComponent.cs
+++ comp361/Assets/Scripts/classComponents/GameComponent.cs
@@ -37,7 +37,59 @@ public class GameComponent : MonoBehaviour {
 	}
 
 	public void newGame(PlayerComponent[] participants) {
-		/* TODO */
+        var firstPlayer = participants[0];
+        setRemainingPlayers(participants);
+        setCurrentPlayer(firstPlayer);
+        Random r = new Random();
+        for (int i = 0; i < 300; i++)
+        {            
+            TileComponent t = new TileComponent(r.Next(0, participants.Length + 1));
+            _mapTiles.Add(t);
+        }
+        for (int i = 0; i < 60; i++)
+        {
+            TileComponent forestTile = _mapTiles[r.Next(0, 300)];
+            forestTile.SetLandType(LandType.FOREST);
+        }
+        for (int i = 0; i < 60; i++)
+        {
+            TileComponent meadowTile = _mapTiles[r.Next(0, 300)];
+            forestTile.setLandType(LandType.MEADOW);
+        }
+        foreach (var tile in _mapTiles)
+        {
+            int playerIndex = getInitialPlayerIndex();
+
+            if (playerIndex < participants.Length)
+            {
+                var region = mapTiles.breadthFS();
+                if (region.Length < 3)
+                {
+                    foreach (var rTile in region)
+                    {
+                        rTile.setInitializingValue(participants.Length);
+                    }
+                }
+                else
+                {
+                    foreach (var rTile in region)
+                    {
+                       var regOccupant = rTile.getOccupantType();
+                       if (regOccupant != OccupantType.VILLAGE)
+                       {
+                           var player = participants[playerIndex];
+                           var newHovel = new VillageComponent(VillageType.HOVEL);
+                           player.add(newHovel);
+                           newHovel.associate(rTile);
+                           newHovel.addGold(7);
+                           var newPeasant = newHovel.hireVillager(UnitType.PEASANT);
+                           int villagerTile = region[1];
+                           newPeasant.associate(rTile);
+                       }
+                    }
+                }
+            }
+        }
 	}
 
 	public void removePlayer(PlayerComponent player) {
