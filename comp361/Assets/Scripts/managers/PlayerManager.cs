a9218663cd49f9beb009b516444314beac9d9e38
Marc Jarvis <jarvis.marc@gmail.com>
2015-02-28 19:26:49 -0500

    Added the ability to create a new PlayerProfile.
    	modified:   comp361/Assets/Prefabs/UI/Menu_InGame.prefab
    	modified:   comp361/Assets/Prefabs/UI/Menu_NewProfile.prefab
    	modified:   comp361/Assets/Prefabs/UI/Menu_PlayerProfile.prefab
    	modified:   comp361/Assets/Prefabs/UI/Menu_Title.prefab
    	modified:   comp361/Assets/Scenes/Main.unity
    	modified:   comp361/Assets/Scripts/classComponents/PlayerComponent.cs
    	modified:   comp361/Assets/Scripts/managers/PlayerManager.cs

--- comp361/Assets/Scripts/managers/PlayerManager.cs
+++ comp361/Assets/Scripts/managers/PlayerManager.cs
@@ -1,8 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerManager : MonoBehaviour {
 	ArrayList _players = new ArrayList();
+
+	//Temporary variables for use in New Profile menu
+	public Text _inputUsername;
+	public Text _inputPassword;
 
 	// Use this for initialization
 	void Start () {
@@ -14,11 +19,43 @@ public class PlayerManager : MonoBehaviour {
 	
 	}
 
-	public void AddPlayer(GameObject playerProfile){
+	public void AddPlayer(PlayerComponent playerProfile){
 		_players.Add(playerProfile);
 	}
 
-	public GameObject GetPlayer(int index){
-		return (GameObject)_players[index];
+	public PlayerComponent GetPlayer(int index){
+		return (PlayerComponent)_players[index];
 	}
+
+	#region New Profile Menu commands
+
+	/// <summary>
+	/// Sets the current username in New Profile menu.
+	/// </summary>
+	public void SetUsername(Text name){
+		_inputUsername = name;
+	}
+
+	/// <summary>
+	/// Sets the current password in New Profile menu.
+	/// </summary>
+	public void SetPassword(Text password){
+		_inputPassword = password;
+	}
+
+	/// <summary>
+	/// Generates a new PlayerComponent and adds it to the PlayerManager.
+	/// </summary>
+	public void SaveNewPlayer(){
+		if(_inputUsername.text.Length > 0 && _inputPassword.text.Length > 0){
+			PlayerComponent newPlayer = new PlayerComponent(_inputUsername.text, _inputPassword.text);
+			_players.Add (newPlayer);
+			Debug.Log ("New player added:" + newPlayer.getUserName());
+			//Clear the temporary information
+			_inputUsername = null;
+			_inputPassword = null;
+		}
+	}
+
+	#endregion
 }
