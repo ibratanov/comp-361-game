7e0f10ec6100ef12cd1a8a100d40a9be3ee0679b
Marc Jarvis <jarvis.marc@gmail.com>
2015-03-08 19:32:50 -0400

    Organized title menus slightly, with GUIManager.
    	modified:   comp361/Assets/Prefabs/Singletons/GUIManager.prefab
    	new file:   comp361/Assets/Prefabs/UI/Menu_Background.prefab
    	new file:   comp361/Assets/Prefabs/UI/Menu_Background.prefab.meta
    	new file:   comp361/Assets/Prefabs/UI/Menu_Choose_Map.prefab
    	new file:   comp361/Assets/Prefabs/UI/Menu_Choose_Map.prefab.meta
    	modified:   comp361/Assets/Prefabs/UI/Menu_InGame.prefab
    	modified:   comp361/Assets/Prefabs/UI/Menu_InGame.prefab.meta
    	modified:   comp361/Assets/Prefabs/UI/Menu_NewProfile.prefab
    	modified:   comp361/Assets/Prefabs/UI/Menu_PlayerProfile.prefab
    	modified:   comp361/Assets/Prefabs/UI/Menu_Title.prefab
    	modified:   comp361/Assets/Scenes/Main.unity
    	modified:   comp361/Assets/Scripts/managers/GUIManager.cs

--- comp361/Assets/Scripts/managers/GUIManager.cs
+++ comp361/Assets/Scripts/managers/GUIManager.cs
@@ -7,7 +7,7 @@ public class GUIManager : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		for(int i = 0; i < _menus.Length; ++i){
-			if(_menus[i].name.Equals("Menu_Title") || _menus[i].name.Equals("Menu_Background")){
+			if(_menus[i].name.Contains("Menu_Title") || _menus[i].name.Contains("Menu_Background")){
 				_menus[i].SetActive(true);
 			}
 			else{
