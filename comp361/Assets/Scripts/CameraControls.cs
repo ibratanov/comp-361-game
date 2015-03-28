3c4fa61523de837c7bd323646416b00194c8fadb
Anh Chi Bui <t-chbui@microsoft.com>
2015-03-28 13:46:27 -0700

    zoomed-in camera controls (wasd)

---
+++ comp361/Assets/Scripts/CameraControls.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControls : MonoBehaviour {
+
+	public float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveCamera();
+	}
+
+	void moveCamera()
+	{
+		if (Input.GetKey (KeyCode.W))
+		{
+			transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z - speed);
+		}
+		else if (Input.GetKey (KeyCode.S))
+		{
+			transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z + speed);
+		}
+
+		if (Input.GetKey (KeyCode.A))
+		{
+			transform.position = new Vector3 (transform.position.x + speed, transform.position.y, transform.position.z);
+		}
+		else if (Input.GetKey (KeyCode.D))
+		{
+			transform.position = new Vector3 (transform.position.x - speed, transform.position.y, transform.position.z);
+		}
+	}
+}
